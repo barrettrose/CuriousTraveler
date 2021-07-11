56f90cc05dbd3f4a24e0ccf7e2708654b371229a
barrettrose <55859959+barrettrose@users.noreply.github.com>
2021-07-10 22:21:18 -0400

    Implemented/setup script for manual slideshow

---
+++ public/javascripts/manual-slideshow.js
@@ -0,0 +1,31 @@
+/*
+Note: Since this code is only used once on the home page, it would be ideal to embed the following JavaScript
+directly in the page, however, I am implementing it externally as a learning experience with Node.js/Express.js
+
+All javascript code as well as HTML/CSS used in the creation of the manual slideshow is credited to w3schools.com
+https://www.w3schools.com/howto/howto_js_slideshow.asp
+*/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("cuisine-slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
